d34634e621f1d680118b50941139cf44a8e7dddf
JuanRomeroSarmiento <camilo318@hotmail.com>
2024-05-03 13:12:01 -0500

    Api Documentation improved

--- Web.Api/EndPoints/Menus/MenusEndPoints.cs
+++ Web.Api/EndPoints/Menus/MenusEndPoints.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Menus.GetAllMenuItemsById;
 using Application.Menus.GetByCategory;
 using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using System.Runtime.InteropServices;
 
 namespace Web.Api.EndPoints.Menus;
@@ -9,28 +10,40 @@ public static class MenusEndPoints
 {
     public static void MapMenuEndPoints(this IEndpointRouteBuilder app)
     {
-        var group = app.MapGroup("api/menus");
+        var group = app.MapGroup("api/menus")
+            .WithTags("Menus");
 
-        group.MapGet("{id}",GetAllMenuItemsByIdMenu);
-        group.MapGet("{menuid}/category/{categoryid}", GetMenuItemsByCategoryId);
+        group.MapGet("{id}/menuitems/",GetAll)
+            .WithName("GetAllMenuItemsByMenuId")
+            .WithOpenApi(op => new(op)
+            {
+                Description = "Operation to get all menu items by menu id"
+            });
+
+        group.MapGet("{menuid}/category/{categoryid}/menuitems/", GetByCategory)
+            .WithName("GetMenuItemsByCategoryId")
+            .WithOpenApi(op => new(op)
+            {
+                Description = "Operation to get all menu items associated with a specific menu id filtered by category id"
+            });
     }
 
-    public static async Task<IResult> GetAllMenuItemsByIdMenu(
+    public static async Task<Ok<AllMenuItemsResponse>> GetAll(
         Guid id,
         ISender sender)
     {
         var query = new GetAllMenuItemsByIdQuery(id);
         var menuItems = await sender.Send(query);
-        return Results.Ok(menuItems);
+        return TypedResults.Ok(menuItems.Value);
     }
 
-    public static async Task<IResult> GetMenuItemsByCategoryId(
+    public static async Task<Ok<MenuItemsByCategoryResponse>> GetByCategory(
         Guid menuid,
         Guid categoryid,
         ISender sender)
     {
         var query = new GetMenuItemsByCategoryIdQuery(menuid, categoryid);
         var menuItems = await sender.Send(query);
-        return Results.Ok(menuItems);
+        return TypedResults.Ok(menuItems.Value);
     }
 }
--- Web.Api/EndPoints/Orders/OrdersEndpoints.cs
+++ Web.Api/EndPoints/Orders/OrdersEndpoints.cs
@@ -2,7 +2,9 @@
 using Application.Orders.PlaceOrder;
 using Application.Orders.Remove;
 using Application.Orders.Update;
+using Domain.Shared;
 using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Web.Api.Filters;
 
@@ -12,24 +14,51 @@ public static class OrdersEndpoints
 {
     public static void MapOrderEndPoints(this IEndpointRouteBuilder app)
     {
-        var group = app.MapGroup("api/orders");
+        var group = app.MapGroup("api/orders")
+            .WithTags("Orders");
 
-        group.MapGet("", GetOrders);
-        group.MapPost("", PlaceOrder)
-            .AddEndpointFilter<ValidationFilter<PlaceOrderRequest>>();
-        group.MapPut("", UpdateOrder)
-            .AddEndpointFilter<ValidationFilter<UpdateOrderRequest>>(); ;
-        group.MapDelete("{id}", DeleteOrder);
+        group.MapGet("", GetAll)
+            .WithName("GetAllOrders")
+            .WithOpenApi(op => new(op)
+            {
+                Description = "Operation to get all orders registered in the system " +
+                "paginated every 10 elements."
+            });
+
+        group.MapPost("", Create)
+            .AddEndpointFilter<ValidationFilter<PlaceOrderRequest>>()
+            .WithName("CreateOrder")
+            .WithOpenApi(op => new(op)
+            {
+                Description = "Operation to create a new order in the system." +
+                "Orders without items or with repeated items are not allowed."
+            });
+
+        group.MapPut("", Update)
+            .AddEndpointFilter<ValidationFilter<UpdateOrderRequest>>()
+            .WithName("UpdateOrder")
+            .WithOpenApi(op => new(op)
+            {
+                Description = "Operation to update an existing order in the system." +
+                "Updates without items or with repeated items are not allowed."
+            });
+
+        group.MapDelete("{id}", Delete)
+            .WithName("DeleteOrder")
+            .WithOpenApi(op => new(op)
+            {
+                Description = "Operation to delete an existing order in the system."
+            });
     }
 
-    public static async Task<IResult> GetOrders(ISender sender)
+    public static async Task<Ok<OrdersResponse>> GetAll(ISender sender)
     {
         var query = new GetAllOrdersQuery();
         var orders = await sender.Send(query);
-        return Results.Ok(orders);
+        return TypedResults.Ok(orders.Value);
     }
 
-    public static async Task<IResult> PlaceOrder(
+    public static async Task<Results<Ok<PlacedOrderCostResponse>, BadRequest<Error>>> Create(
         PlaceOrderRequest request,
         ISender sender)
     {
@@ -45,10 +74,12 @@ public static class OrdersEndpoints
 
         var result = await sender.Send(command);
 
-        return result.IsSuccess ? Results.Ok(result.Value) : Results.BadRequest(result.Error);
+        return result.IsSuccess ? 
+            TypedResults.Ok(result.Value) :
+            TypedResults.BadRequest(result.Error);
     }
 
-    public static async Task<IResult> UpdateOrder(
+    public static async Task<Results<Ok, BadRequest<Error>>> Update(
         UpdateOrderRequest request,
         ISender sender)
     {
@@ -66,15 +97,19 @@ public static class OrdersEndpoints
 
         var result = await sender.Send(command);
 
-        return result.IsSuccess ? Results.Ok() : Results.BadRequest(result.Error);
+        return result.IsSuccess ? 
+            TypedResults.Ok() : 
+            TypedResults.BadRequest(result.Error);
     }
 
-    public static async Task<IResult> DeleteOrder(
+    public static async Task<Results<Ok, BadRequest<Error>>> Delete(
         Guid id,
         ISender sender)
     {
         var command = new DeleteOrderCommand(id);
         var result = await sender.Send(command);
-        return result.IsSuccess ? Results.Ok() : Results.BadRequest(result.Error);
+        return result.IsSuccess ? 
+            TypedResults.Ok() : 
+            TypedResults.BadRequest(result.Error);
     }
 }
